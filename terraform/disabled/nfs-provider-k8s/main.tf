028b172754e4e469fae43c98d337b2dd8e325c2b
Jose Mario Gomez Hernandez <jgomezhernandez@splunk.com>
2023-08-22 01:34:07 -0600

    Testing add mssql 2019

--- terraform/disabled/nfs-provider-k8s/main.tf
+++ terraform/disabled/nfs-provider-k8s/main.tf
@@ -1,16 +1,22 @@
 data "template_file" "chart_values" {
-  template = file("chart-values.yml")
+  vars = {
+    SERVER = "34.216.204.56"
+    PATH = "/var/uolshare/"
+  }
 }
-##helm install uol-nfs-provider --set nfs.server=34.216.204.56 --set nfs.path=/var/uolshare/ stable/nfs-client-provisioner
 
 resource "helm_release" "nfs-provider" {
   chart = "nfs-subdir-external-provisioner/nfs-subdir-external-provisioner"
   name = "nfs-provider"
   repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner"
-  #version   = "3.21.0"
-  #namespace = var.namespace
+}
 
-  values = [
-    data.template_file.chart_values.rendered
-  ]
+set {
+  name  = "nfs.server"
+  value = data.template_file.chart_values.vars.SERVER
+}
+
+set {
+  name  = "nfs.path"
+  value = data.template_file.chart_values.vars.PATH
 }
