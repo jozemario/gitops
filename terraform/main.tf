50eaf3c4765089d1dbf46dd7d2204acb593238c2
Jose Mario Gomez Hernandez <jgomezhernandez@splunk.com>
2023-08-21 22:37:56 -0600

    Testing add mssql 2019

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,37 +3,12 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-resource "kubernetes_namespace" "nfs-provider" {
-  metadata {
-    name = "dbs"
-  }
-}
-
  variable "subject" {
     type = string
     default = "tfctl-rc"
     description = "Subject to hello"
  }
 
-variable "namespace" {
-  value = "dbs"
-}
-
  output "hello_world" {
    value = "hey hey ya, ${var.subject}!"
  }
-
-data "template_file" "chart_values" {
-  template = file("./disabled/nfs-provider-k8s/chart-values.yml")
-}
-
-resource "helm_release" "nfs-provider" {
-  chart = "nfs-subdir-external-provisioner/nfs-subdir-external-provisioner"
-  name = "nfs-provider"
-  repository = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner"
-  namespace = var.namespace
-
-  values = [
-    data.template_file.chart_values.rendered
-  ]
-}
