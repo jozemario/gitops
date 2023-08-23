6a9d2550670bdc27596ddcf959fbf2d185f18aff
Jose Mario Gomez Hernandez <jgomezhernandez@splunk.com>
2023-08-23 00:39:23 -0600

    Testing add mssql 2019

--- terraform/disabled/module.tf
+++ terraform/disabled/module.tf
@@ -1,8 +1,8 @@
 
-#module "nfs-provider-k8s" {
-#  source = "./nfs-provider-k8s"
-#}
-
-module "mssql-server-k8s" {
-  source = "./mssql-server-k8s"
+module "nfs-provider-k8s" {
+  source = "./nfs-provider-k8s"
 }
+
+#module "mssql-server-k8s" {
+#  source = "./mssql-server-k8s"
+#}
