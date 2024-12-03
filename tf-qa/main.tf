24888b8a915acafc59a055d55583af425a2780ea
JozeMario <josezero1@gmail.com>
2024-12-02 21:17:39 -0600

    Added envs qa

--- tf-qa/main.tf
+++ tf-qa/main.tf
@@ -15,112 +15,54 @@ variable "branch" {
    description = "QA Branch"
 }
 
-
-# Create MariaDB Secret
-resource "kubernetes_secret" "mariadb" {
-  metadata {
-    name      = "mariadb-secret"
-    namespace = "dev"
-  }
-
-  data = {
-    MYSQL_ROOT_PASSWORD = "UGFzc3dvcmQxMjMh" # base64 encoded "Password123!"
-    MYSQL_DATABASE     = "testdb"
-  }
-}
-
-# Create MariaDB Deployment
-resource "kubernetes_deployment" "mariadb" {
-  metadata {
-    name      = "mariadb"
-    namespace = "dev"
-    labels = {
-      app = "mariadb"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "mariadb"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "mariadb"
-        }
-      }
-
-      spec {
-        container {
-          image = "mariadb:10.6"
-          name  = "mariadb"
-
-          port {
-            container_port = 3306
-          }
-
-          env {
-            name = "MYSQL_ROOT_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.mariadb.metadata[0].name
-                key  = "MYSQL_ROOT_PASSWORD"
-              }
-            }
-          }
-
-          env {
-            name = "MYSQL_DATABASE"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.mariadb.metadata[0].name
-                key  = "MYSQL_DATABASE"
-              }
-            }
-          }
-          resources {
-            limits = {
-              cpu    = "500m"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "256Mi"
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-# Create MariaDB Service
-resource "kubernetes_service" "mariadb" {
-  metadata {
-    name      = "mariadb"
-    namespace = "dev"
-  }
-  spec {
-    selector = {
-      app = kubernetes_deployment.mariadb.metadata[0].labels.app
-    }
-    port {
-      port        = 3306
-      target_port = 3306
-    }
-    type = "ClusterIP"
-  }
-}
-
 output "hello_world" {
   value = "Welcome to QA - ${var.branch} branch!"
 }
 
-output "mariadb_connection_string" {
-  value = "mysql -h ${kubernetes_service.mariadb.metadata[0].name}.dev.svc.cluster.local -P 3306 -u root -p"
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = dev
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "NodePort"
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+  }
 }
