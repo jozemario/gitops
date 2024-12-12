fa7ad7b9977368f70700bf2b04c4c814097741e5
JozeMario <josezero1@gmail.com>
2024-12-12 16:12:40 -0600

    Added mariaDB to QA

--- tf-qa/shared/main.tf
+++ tf-qa/shared/main.tf
@@ -5,7 +5,7 @@ locals {
       mariadb_database      = "mghcloud"
       mariadb_user          = "mghcloud"
       wordpress_db_name     = "wordpress"
-      wordpress_db_user     = "mghcloud"
+      wordpress_db_user     = "root"
       wordpress_db_password = "change_me"
       wordpress_db_host     = "201.205.178.45"
       wordpress_db_port     = "30005"
