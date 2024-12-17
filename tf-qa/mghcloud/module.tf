22a1b076601d9543d23d812b317a69352f937f7d
JozeMario <josezero1@gmail.com>
2024-12-16 23:26:58 -0600

    Added vault fixed to QA

--- tf-qa/mghcloud/module.tf
+++ tf-qa/mghcloud/module.tf
@@ -14,6 +14,10 @@ module "wordpress" {
   source = "./wordpress"
 }
 
-# module "vault" {
-#   source = "./vault"
-# }
+module "vault" {
+  source = "./vault"
+}
+
+module "keycloak" {
+  source = "./keycloak"
+}
